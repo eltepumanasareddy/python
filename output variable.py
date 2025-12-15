7c1ff8bd1a35f767b398df0447b3e14cc05724f6
eltepumanasareddy <eltepumanasareddy@gmail.com>
2025-12-15 23:19:54 +0530

    mui

--- output variable.py
+++ output variable.py
@@ -1,5 +1,11 @@
+#x ="iam "
+#y="so "
+#z="beautiful"
+#print(x + y + z)
+#Notice the space character after "Python " and "is ", without them the result would be "Pythonisawesome".
+
+
 x ="iam "
 y="so "
 z="beautiful"
-print(x + y + z)
-#Notice the space character after "Python " and "is ", without them the result would be "Pythonisawesome".
+print(x , y , z)
