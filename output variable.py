ef64fb81d351c8b63e79b501a6d4e909a194aa76
eltepumanasareddy <eltepumanasareddy@gmail.com>
2025-12-15 23:21:59 +0530

    output

--- output variable.py
+++ output variable.py
@@ -5,7 +5,12 @@
 #Notice the space character after "Python " and "is ", without them the result would be "Pythonisawesome".
 
 
-x ="iam "
-y="so "
-z="beautiful"
-print(x , y , z)
+#x ="iam "
+#y="so "
+#z="beautiful"
+#print(x , y , z)
+
+
+x = 5
+y = 10
+print(x + y)
