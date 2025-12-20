d060773683a885c50dab6a460af2f1e23edcb48b
eltepumanasareddy <eltepumanasareddy@gmail.com>
2025-12-20 22:47:59 +0530

    bytes

--- bytearray.py
+++ bytearray.py
@@ -7,3 +7,29 @@ print(x)
 print(type(x)) 
 
 
+#byte
+x=b"hello"
+print(x)
+print(tyoe(x))
+
+#memory view
+x = memoryview(bytes(5))
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+
+#none
+x = None
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+
+
