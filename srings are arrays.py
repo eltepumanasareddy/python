e4de5742f64dc4963c0bea6c942584b0a7f0809e
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-01-03 23:19:13 +0530

    arr

--- srings are arrays.py
+++ srings are arrays.py
@@ -23,3 +23,22 @@ print(b[:5])
 #negative indexing
 b = "Hello, World!"
 print(b[-5:-2])
+#upper case
+a = "Hello, World!"
+print(a.upper())
+#lower case
+a = "Hello, World"
+print(a.lower())
+
+#remove whitespace
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+
+#replace string
+a = "Hello, World!"
+print(a.replace("H", "M"))
+
+#split string
+a = "Hello, World!"
+print(a.split(","))
+print(b)
