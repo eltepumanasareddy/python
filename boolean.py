59e598658b85175f80a1211ff3c15abc148eb029
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-01-05 22:52:09 +0530

    bo

--- boolean.py
+++ boolean.py
@@ -1,4 +1,18 @@
-print(4<5)
-print(4>5)
-print(4==5)
-print(4==4)
+#boolean
+#print(4<5)
+#print(4>5)
+#print(4==5)
+#print(4==4)
+
+#if statement
+a=200
+b=199
+if b>a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a")
+
+#evaluate a string and a number:
+print(bool("hello"))
+print(bool(16))
+
