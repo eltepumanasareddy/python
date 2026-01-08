3ae9492968dc7c2af3438031ede72f4dc8b90274
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-01-08 19:47:24 +0530

    ope

--- day1/operaters.py
+++ day1/operaters.py
@@ -1,28 +1,6 @@
-sum1=100
-sum2=sum1+20
-sum3=sum2+sum1
+sum1= 100 + 500
+sum2= sum1 +20
+sum3= sum1 + sum2
 print(sum1)
 print(sum2)
 print(sum3)
-
-#operaters
-x = 15
-y = 4
-
-print(x + y)
-print(x - y)
-print(x * y)
-print(x / y)
-print(x % y)
-print(x ** y)#same as 2*2*2*2*2(#Exponentiation)
-print(x // y)#the floor division // rounds the result down to the nearest whole number.
-#floor division
-
-#Assignment Operators
-x = 5
-print(x)
-
-
-
-
-
