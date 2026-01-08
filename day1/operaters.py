d45683536b435102c1ef075bb32151d4bbea8f3d
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-01-08 20:53:08 +0530

    assigment

--- day1/operaters.py
+++
@@ -1,6 +0,0 @@
-sum1= 100 + 500
-sum2= sum1 +20
-sum3= sum1 + sum2
-print(sum1)
-print(sum2)
-print(sum3)
---
+++ day1/operators.py
@@ -0,0 +1,67 @@
+sum1= 100 + 500
+sum2= sum1 +20
+sum3= sum1 + sum2
+print(sum1)
+print(sum2)
+print(sum3)
+
+#arthematic operators
+x=2
+y=5
+print(x + y)
+print(x -y)
+print(x * y)
+print(x /y)
+print(x//y)#the floor division // rounds the result down to the nearest whole number
+print(x**y) #same as 2*2*2*2*2 
+print(x%y)
+
+#assignment operators
+#1
+x=5
+print(x)
+
+#2
+x=5
+x +=3 #(x=x+3)
+print(x)
+
+#3
+x=5
+x -=3 #(x=x-3)
+print(x)
+
+#4
+x=5
+x*=3 #(x=x*3)
+print(x)
+
+#5
+x=3
+x/=2
+print(x)
+
+#6
+x=4
+x//=5
+print(x)
+
+#7
+x=5
+x**=6#(5*5)
+print(x)
+
+#8
+x=5
+x&=4
+print(x)
+
+
+
+
+
+
+
+ 
+
+
