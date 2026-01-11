0deafe425c2a50dfed50f7978a4052d1a79c71c0
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-01-11 21:47:28 +0530

    values

--- day1/identify .py
+++ day1/identify .py
@@ -4,3 +4,13 @@ z=x#z = x means z points to the same list as x
 print(x is y)
 print(x == y)
 print(x is z)
+
+#1
+x=["apple","banana"]
+y=["apple","banana"]
+z=x
+print(x is not y)
+print(x==y)
+print(x==z)#values equal
+print(x!=y)#same value thats why true will be come
+print(x is not z)## returns False because z is the same object as x
