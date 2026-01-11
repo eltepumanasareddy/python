4475a16efb23c641c5b9592880ad816a23f91c20
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-01-11 21:35:56 +0530

    banana

--- day1/identify .py
+++ day1/identify .py
@@ -2,3 +2,5 @@ x=["apple","banana"]
 y=["apple","banana"]#y is another list with the same values, but it is a different list in memory
 z=x#z = x means z points to the same list as x
 print(x is y)
+print(x == y)
+print(x is z)
