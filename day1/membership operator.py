3488b62c5eb85db38de08656fd393eb38a04f4bd
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-01-12 21:25:13 +0530

    text

--- day1/membership operator.py
+++ day1/membership operator.py
@@ -5,3 +5,13 @@ print("manasa" in x)
 x=["manasa","reddy"]
 print("manasa" not in x)
 print("eltepu" not in x)
+
+#2
+#text is a variable
+#It stores a string value
+#The string "Hello World" is assigned to text
+text="Hello World"
+print("W" in text)
+print("hello" in text)
+print("z" not in text)
+
