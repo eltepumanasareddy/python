9b0bc99f3a47b5c6b7289d18de865ee143248251
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-01-13 21:13:31 +0530

    not

--- day1/Bitwise operator.py
+++ day1/Bitwise operator.py
@@ -1,11 +1,23 @@
-print(6 & 3)#sets each bit to 1 if both bits are 1	
+#AND
+# print(6 & 3)#sets each bit to 1 if both bits are 1	
 #6 = 0000000000000110
 #3 = 0000000000000011
 #--------------------
 #2 = 0000000000000010
 
+#OR
 print(6 | 4)#Sets each bit to 1 if one of two bits is 1
 #6 = 0000000000000110
 #3 = 0000000000000011
 #--------------------
 #7 = 0000000000000111
+
+#XOR
+print(6 ^ 4)
+# 6 = 0110
+# 3 = 0011
+# --------
+# 5 = 0101
+
+#NOT
+print(~4)
