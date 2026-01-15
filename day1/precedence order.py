aac513c826c0c464c9ba83a0213b35f054a63006
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-01-15 20:52:41 +0530

    logic

--- day1/precedence order.py
+++ day1/precedence order.py
@@ -5,6 +5,12 @@ print(5 + 4 - 3 +4)
 print(2 + 3 * 4)
 print(100 + ~3)
 #Bitwise left and right shifts
-print(8>>4-2)
+print(8>>4-2)#Bitwise right shift has a lower precedence than subtraction, and we need to calculate the subtraction first.
+#The calculation above reads 8 >> 2 = 2
+#More explanation:
+#The >> operator moves each bit the specified number of times to the right. Empty holes at the left are filled with 0's.
 #Bitwise AND
 print(5&3+1)
+#bitwise XOR
+print(6^2+1)
+print(6|2+1)
