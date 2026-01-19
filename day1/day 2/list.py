903d0b700ccbbb9b84ba31dacb14ea240f8204db
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-01-19 21:18:39 +0530

    banana

--- day1/day 2/list.py
+++ day1/day 2/list.py
@@ -4,3 +4,12 @@ print(thislist)
 #length
 thislist=["apple","banana","apple","banana"]
 print(len(thislist))
+
+#String, int and boolean data types
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+print(list1)
+print(list2)
+print(list3)
