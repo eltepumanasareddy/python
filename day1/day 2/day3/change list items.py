9b20c9b7ea08a5f87c9a05cd150cc2f229ad8401
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-01-31 21:02:45 +0530

    water

--- day1/day 2/day3/change list items.py
+++ day1/day 2/day3/change list items.py
@@ -6,3 +6,8 @@ print(list)
 list=["apple","banana","cherry"]
 list[1:3]=["black","green"]
 print(list)
+
+#2
+thislist = ["apple", "banana", "cherry"]
+thislist[1:3] = ["watermelon"]
+print(thislist)
