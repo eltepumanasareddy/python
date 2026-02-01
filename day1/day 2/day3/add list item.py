f3c75fe9c14046923416d985ea5bfb89d4eaffb6
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-02-01 12:47:20 +0530

    jdjala

--- day1/day 2/day3/add list item.py
+++ day1/day 2/day3/add list item.py
@@ -1,3 +1,17 @@
 list=["apple","banana","cherry"]
-list.append("orange")
+list.append("orange")#append=always adds an element to the end of a list
 print(list)
+#1
+thislist=["apple","banana","cherry"]
+thislist.insert(3,"orange")
+print(thislist)
+#2
+thislist= ["apple","banana","cherry"]
+tropical=["mango","papaya","banana"]#tropical list itself is not nested; its elements are merged
+thislist.extend(tropical)#extend() adds all elements of one list to another list
+print(thislist)
+#3
+thislist=["apple","banana","cherry"]
+thistuple=("kiwi","orange")
+thislist.extend(thistuple)
+print(thislist)
