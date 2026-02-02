f826da1564e130053e581be79e115a90a0636283
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-02-02 23:47:31 +0530

    one

--- day1/day 2/day3/day4/day5/loop list.py
+++ day1/day 2/day3/day4/day5/loop list.py
@@ -1,3 +1,8 @@
 thislist = ["apple", "banana", "cherry"]
-for x in thislist:
+for x in thislist:#for=one at a time
  print(x)
+ 
+#1
+list=["apple","banana","cherry"]
+for i in range(len(thislist)):
+ print(thislist[i])
