bbbfe2f9b24d8859cabe2438fd1bca906cb96847
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-02-04 19:42:26 +0530

    animal

--- day1/day 2/day3/day4/day5/day6/day7/syntax.py
+++ day1/day 2/day3/day4/day5/day6/day7/syntax.py
@@ -1,3 +1,8 @@
 animals=["goat","tiger","lion","elephant"]
 newlist =[x for x in animals if x !="goat"]
 print(newlist)
+
+#1
+fruits=["apple","banana","grapes"]
+newlist=[x for x in fruits]
+print(newlist)
