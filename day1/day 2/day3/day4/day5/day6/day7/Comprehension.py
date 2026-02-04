213b1d9f9ac399018e91208c2fbfc945e27812c9
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-02-04 19:07:58 +0530

    cherry

--- day1/day 2/day3/day4/day5/day6/day7/Comprehension.py
+++ day1/day 2/day3/day4/day5/day6/day7/Comprehension.py
@@ -4,3 +4,9 @@ for x in fruits:
     if "c" in x:
         newlist.append(x)
         print(newlist)
+
+#1
+fruits=["apple","banana","cherry","kiwi","mango"]
+newlist=[x for x in fruits if "r" in x]
+print(newlist)
+
