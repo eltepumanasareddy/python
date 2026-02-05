c49f7d67b43997af90860fabf442c1b2c201627b
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-02-05 08:29:21 +0530

    hello

---
+++ day1/day 2/day3/day4/day5/day6/day7/day8/expression.py
@@ -0,0 +1,8 @@
+fruits=["apple","banana","kiwi","guva"]
+newlist=[x.upper() for x in fruits]
+print(newlist)
+
+#1
+fruits=["apple","banana","kiwi","guva"]
+newlist=['hello' for x in fruits]
+print(newlist)
