657a3f4f7e6e6bd6890868e28803691f8780fe5b
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-02-05 21:18:16 +0530

    try

--- day1/day 2/day3/day4/day5/day6/day7/day8/sort.py
+++ day1/day 2/day3/day4/day5/day6/day7/day8/sort.py
@@ -11,3 +11,8 @@ print(list)
 list=["orange","mango","kiwi","pineapple","banana"]
 list.sort(reverse = True)
 print(list)
+
+#3
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(reverse=True)
+print(thislist)
