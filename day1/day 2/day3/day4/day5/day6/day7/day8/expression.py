de10182b5a0617857f9143a9090290dcc86b3e90
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-02-05 08:34:20 +0530

    else

--- day1/day 2/day3/day4/day5/day6/day7/day8/expression.py
+++ day1/day 2/day3/day4/day5/day6/day7/day8/expression.py
@@ -6,3 +6,9 @@ print(newlist)
 fruits=["apple","banana","kiwi","guva"]
 newlist=['hello' for x in fruits]
 print(newlist)
+
+#2
+fruits=["apple","banana","kiwi","guva"]
+newlist=[x if x!="banana" else "orange" for x in fruits]
+print(newlist)
+
