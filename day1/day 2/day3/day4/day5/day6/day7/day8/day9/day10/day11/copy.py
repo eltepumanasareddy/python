b45a6ef0a376ff14bdd71327d43fb44f2b635ee8
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-02-08 22:07:16 +0530

    ugfido

--- day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/copy.py
+++ day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/copy.py
@@ -2,7 +2,12 @@ thislist = ["apple", "banana", "cherry"]
 mylist = thislist.copy()
 print(mylist)
 
-#1
+#1use list() method
 thislist=["apple","banana","cherry"]
 mylist=list(thislist)
+print(thislist)
+
+#2 use the slice operator
+thislist=["apple","banana","cherry"]
+mylist=thislist[:]
 print(thislist)
