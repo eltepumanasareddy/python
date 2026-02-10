9c84000cecde99a157e005d8fb403aa733653a42
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-02-10 20:55:26 +0530

    sathvika

--- day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/tuples.py
+++ day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/tuples.py
@@ -4,3 +4,10 @@ print(thistuple)
 #1
 thistuple = ("cherry","banana","mango","apple")
 print(len(thistuple))
+
+#2
+thistuple = ("cherry",)
+print(type(thistuple))
+thistuple = ("apple")
+print(type(thistuple))
+ 
