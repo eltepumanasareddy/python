c40cf445bf30210b165e201e7f1fa9d5d5773597
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-02-10 22:53:35 +0530

    temper

--- day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/access.py
+++ day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/access.py
@@ -1,2 +1,10 @@
 thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
 print(thistuple[:4])
+
+#1
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:])
+
+#range of negative index
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-5:-3])
