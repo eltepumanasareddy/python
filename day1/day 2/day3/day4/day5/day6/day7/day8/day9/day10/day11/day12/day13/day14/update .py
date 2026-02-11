0b0cc9b49aa9bf5d1db3813771f308b2f5b34703
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-02-11 22:40:37 +0530

    hfjdkl

--- day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/update .py
+++ day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/update .py
@@ -1,5 +1,13 @@
+#change tuple values
 x=("apple","banana","cherry")
 y=list(x)
 y[1]="kiwi"
 x=tuple(y)
 print(x)
+
+#add items
+thislist=("apple","banana","cherry")
+y=list(thislist)
+y.append("guvva")
+thistuple=tuple(y)
+print(thistuple)
