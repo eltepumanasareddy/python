5a33144d6c14765d6a0dcfce454e6bb3d6064878
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-02-13 22:58:14 +0530

    collection

--- day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/Asterisk.py
+++ day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/Asterisk.py
@@ -3,3 +3,12 @@ fruits=("apple","banana","cherry","straberry","rasberry")
 print(green)
 print(yellow)
 print(red)
+
+#1 tropic=collects everything in between into a list
+fruits=("apple","banana","cherry","straberry","rasberry")
+(green,*tropic,red)=fruits
+print(green)
+print(yellow)
+print(red)
+
+
