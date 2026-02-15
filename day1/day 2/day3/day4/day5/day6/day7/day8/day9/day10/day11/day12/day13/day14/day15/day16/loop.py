1296c064a5287e9c14d4d01e6cd51afc6ded128b
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-02-15 20:58:43 +0530

    chjkl;

--- day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/loop.py
+++ day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/loop.py
@@ -24,3 +24,11 @@ print(thistuple3)
 fruits=("apple","banana","cherry")
 mytuple=fruits*2
 print(mytuple)
+
+
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+
+print(x)
