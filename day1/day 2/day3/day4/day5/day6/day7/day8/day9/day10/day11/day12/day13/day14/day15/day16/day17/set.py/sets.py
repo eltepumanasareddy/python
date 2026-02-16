e973a317581f13dde4e6c126f8a81cd6e3a16675
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-02-16 22:01:06 +0530

    lengthset

--- day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/set.py/sets.py
+++ day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/set.py/sets.py
@@ -5,3 +5,20 @@ print(thisset)
 #false=0
 thisset={"apple","banana","cherry",True,1,2}
 print(thisset)
+
+thisset={"apple","banana","cherry",False,True,0}
+print(thisset)
+
+thisset={"apple","banana","cherry"}
+print(len(thisset))
+
+set1={"apple","banana","cherry"}
+set2={1,2,3,4,5}
+set3={True,False,False}
+print(set1)
+print(set2)
+print(set3)
+
+set1 = {"abc", 34, True, 40, "male"}
+print(set1)
+
