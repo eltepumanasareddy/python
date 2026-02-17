f22276f42364b0a341901be022feaac39da65cfe
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-02-17 22:10:00 +0530

    hgdsjk

--- day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/removes.py
+++ day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/removes.py
@@ -2,3 +2,14 @@ thisset={"apple","banana","cherry"}
 x=thisset.pop()
 print(x)
 print(thisset)
+
+#1
+thisset={"apple","banana","cherry"}
+thisset.clear()
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.discard("banana")
+
+print(thisset)
