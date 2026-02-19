f16fd323fb905e4eb7a0284180e00fdd1355c95d
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-02-19 23:09:42 +0530

    fdsjskr

--- day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/joins.py
+++ day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/joins.py
@@ -8,3 +8,11 @@ set1 = {"a", "b", "c"}
 set2 = {1, 2, 3}
 set3 = set1 | set2
 print(set3)
+
+#2
+set1={"a","b","c"}
+set2={1,2,3}
+set3={"john","Elena"}
+set4={"apple","banana","cherry"}
+myset=set1.union(set2, set3, set4)
+print(myset)
