0d68cd759e36687653b0c2df410d837aa5bd928c
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-02-20 19:35:22 +0530

    microsofts

--- day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/update.py
+++ day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/update.py
@@ -4,14 +4,21 @@ y={"google","microsoft","apple"}
 z=x.intersection(y)
 print(z)
 
-#difference
+#difference is exist in one set
 x={"apple","banana","cherry"}
 y={"google","microsoft","apple"}
 z=x.difference(y)
 print(z)
 
-#symmetric difference
+#symmetric difference is a not same values are presented 
 x={"apple","banana","cherry"}
 y={"google","microsoft","apple"}
 z=x.symmetric_difference(y)
 print(z)
+
+#2
+set1={"apple","banana","cherry"}
+set2={"google","microsoft","apple"}
+set3=set1 & set2
+print(set3)
+
