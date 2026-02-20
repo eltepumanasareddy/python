d6ab0dd5184a7ac95d68f8765cbd5e080bbf1654
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-02-20 20:03:20 +0530

    gfyreui

--- day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/update.py
+++ day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/update.py
@@ -22,3 +22,16 @@ set2={"google","microsoft","apple"}
 set3=set1 & set2
 print(set3)
 
+#3
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+set1.difference_update(set2)
+print(set1)
+
+#4
+set1={"apple","banana","cherry"}
+set2={"google","microsoft","apple"}
+set3=set1^set2
+print(set3)
+
+
