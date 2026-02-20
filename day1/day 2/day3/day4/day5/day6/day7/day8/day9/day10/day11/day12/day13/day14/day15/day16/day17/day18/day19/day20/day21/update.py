0660d6d473cca63d505a89a7eedbe31dbf846cf5
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-02-20 19:19:49 +0530

    diff

--- day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/update.py
+++ day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/update.py
@@ -3,3 +3,15 @@ x={"apple","banana","cherry"}
 y={"google","microsoft","apple"}
 z=x.intersection(y)
 print(z)
+
+#difference
+x={"apple","banana","cherry"}
+y={"google","microsoft","apple"}
+z=x.difference(y)
+print(z)
+
+#symmetric difference
+x={"apple","banana","cherry"}
+y={"google","microsoft","apple"}
+z=x.symmetric_difference(y)
+print(z)
