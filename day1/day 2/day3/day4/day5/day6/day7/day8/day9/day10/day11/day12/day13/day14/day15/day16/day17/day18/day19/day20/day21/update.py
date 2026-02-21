9054e3606ac34fe5fcf6e5bacfe9513fdf242d00
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-02-21 10:27:18 +0530

    frozen

--- day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/update.py
+++ day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/update.py
@@ -34,4 +34,10 @@ set2={"google","microsoft","apple"}
 set3=set1^set2
 print(set3)
 
+#5
+set1={"apple","banana","cherry"}
+set2={"google","microsoft","apple"}
+set1.symmetric_difference_update(set2)
+print(Set1)
+
 
