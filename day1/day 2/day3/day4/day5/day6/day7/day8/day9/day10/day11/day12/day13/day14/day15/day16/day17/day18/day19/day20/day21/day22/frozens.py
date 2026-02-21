c87eabe3c080cfd35ad395651d13ec75d6aa0b27
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-02-21 11:00:57 +0530

    disjoint

--- day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/day22/frozens.py
+++ day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/day22/frozens.py
@@ -1,3 +1,16 @@
+#frozenset is a Once created, you cannot add or remove items.
 x=frozenset({"apple","banana","cherry"})
 print(x)
 print(type(x))
+
+#1
+fs = frozenset({1,2,3})
+cp=fs.copy()
+print(fs)
+print(cp)
+#2
+a=frozenset({1,2})
+b=frozenset({3,4})
+c=frozenset({2,5})
+print(a.isdisjoint(b))
+print(a.isdisjoint(c))
