588be5d547b479423a33f7fa52a5bdc57a7b69b0
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-02-23 22:44:04 +0530

    whites

--- day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/day22/day23/day24/dictionary.py
+++ day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/day22/day23/day24/dictionary.py
@@ -18,3 +18,25 @@ x = car.values()
 print(x) #before the change
 car["color"] = "red"
 print(x) #after the change
+
+#3
+thisdict={
+   "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+} 
+thisdict.update({"year":2020})
+print(thisdict)
+
+#4
+car={
+"brand":"Ford",
+"model":"Mustang",
+"year": 1964
+}
+x=car.keys()
+print(x)
+car["color"]="white"
+print(x)
+
+
