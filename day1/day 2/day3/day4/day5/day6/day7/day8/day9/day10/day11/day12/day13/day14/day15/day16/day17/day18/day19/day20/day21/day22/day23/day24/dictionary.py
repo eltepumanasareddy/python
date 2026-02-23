015edfa887e7cd8925f94803715737beb2e07f3c
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-02-23 19:25:16 +0530

    change

--- day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/day22/day23/day24/dictionary.py
+++ day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/day22/day23/day24/dictionary.py
@@ -3,7 +3,18 @@ car = {
 "model": "Mustang",
 "year": 1964
 }
-x=car.values()#before value
+x=car.values()#before the change
 print(x)
-car["year"]=2020#after value
+car["year"]=2020#after the change
 print(x)
+
+#1
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+x = car.values()
+print(x) #before the change
+car["color"] = "red"
+print(x) #after the change
