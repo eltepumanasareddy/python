bdf0d0d046d0d13a7b76002ea11620dfe01ddde9
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-02-24 22:19:37 +0530

    yreiwop

--- day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/day22/day23/day24/day25/removedict.py
+++ day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/day22/day23/day24/day25/removedict.py
@@ -6,3 +6,23 @@ dict={
 dict.pop("model")
 print(dict)
 
+#1
+dict={
+    "brand": "Ford",
+    "model":"mustrang",
+    "year":2005
+}
+dict.clear()
+print(dict)
+
+dict={
+    "brand": "Ford",
+    "model":"mustrang",
+    "year":2005
+}
+del dict["model"]
+print(dict)
+
+
+
+
