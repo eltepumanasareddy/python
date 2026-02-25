c447fe8513b96dd3c04b3debe28f0c04b767f67a
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-02-25 22:46:26 +0530

    files

--- day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/day22/day23/day24/day25/day26/loops.py
+++ day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/day22/day23/day24/day25/day26/loops.py
@@ -14,3 +14,13 @@ thisdict =	{
 }
 for x in thisdict:
   print(thisdict[x])
+
+#2
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in thisdict.values():
+   print(x)
+
