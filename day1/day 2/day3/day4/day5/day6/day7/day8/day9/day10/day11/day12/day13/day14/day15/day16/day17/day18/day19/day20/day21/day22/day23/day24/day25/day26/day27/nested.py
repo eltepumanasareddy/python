ae51ea17a3268f3761f9e01f361a0b8dcc7951b7
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-02-26 23:16:26 +0530

    children

--- day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/day22/day23/day24/day25/day26/day27/nested.py
+++ day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/day22/day23/day24/day25/day26/day27/nested.py
@@ -13,3 +13,41 @@ myfamily={
 }
 }
 print(myfamily)
+
+#1
+child1={
+    "name":"Email",
+    "year":2005
+}
+child2={
+    "name":"manasa",
+    "year":2006
+}
+child3={
+    "name":"vinay",
+    "year":2006
+}
+myfamily={
+ "child1":child1,
+ "child2":child2,
+ "child3":child3
+}
+print(myfamily)
+
+#2
+myfamily = {
+  "child1" : {
+    "name" : "Email",
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+}
+
+print(myfamily["child2"]["name"])
