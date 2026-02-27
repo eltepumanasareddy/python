7f52061f0d23029fdb18d937c3cdc45d5050f913
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-02-27 22:47:39 +0530

    items

--- day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/day22/day23/day24/day25/day26/day27/day28/nest.py
+++ day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/day22/day23/day24/day25/day26/day27/day28/nest.py
@@ -13,3 +13,24 @@ myfamily={
   }
 }
 print(myfamily["child2"]["name"])
+
+#1
+myfamily = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+}
+for x, obj in myfamily.items():
+    print(x)
+    for y in obj:
+        print(y + ':', obj[y])
+
