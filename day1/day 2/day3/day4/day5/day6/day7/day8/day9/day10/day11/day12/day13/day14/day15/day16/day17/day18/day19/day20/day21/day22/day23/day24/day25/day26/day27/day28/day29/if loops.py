0fb9387d247bede5793b7453fb70d2a82d7e7dc2
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-02-28 20:25:41 +0530

    back

--- day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/day22/day23/day24/day25/day26/day27/day28/day29/if loops.py
+++ day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/day22/day23/day24/day25/day26/day27/day28/day29/if loops.py
@@ -7,3 +7,15 @@ if b>a:
 number=45
 if number>0:
     print("The number is positive & negative")
+
+#2
+age=20
+if age >= 18:
+    print("you are an adult")
+    print("you can vote")
+    print("you have full legal rights")
+
+#3
+is_logged_in = True
+if is_logged_in:
+  print("Welcome back!")
