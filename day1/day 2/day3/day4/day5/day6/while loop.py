4cafdc32d495e627def3d7a7f4ed2bbba077effe
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-03-01 22:45:10 +0530

    prevent

--- day1/day 2/day3/day4/day5/day6/while loop.py
+++ day1/day 2/day3/day4/day5/day6/while loop.py
@@ -1,9 +1,9 @@
-thislist=["apple","banana","grapes"]
+thislist=["cherry","guava","mango"]
 i = 0
 while i < len(thislist):
  print(thislist[i])
  i= i + 1#prevents an infinite loop
 
  #1
- thislist=["apple","banana","cherry"]
+ thislist=["cherry","guava","mango"]
  [print(x) for x in thislist]
