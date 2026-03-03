f5c62cb2b2d7d3c15e813912ddb591753c3e98ff
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-03-03 21:21:19 +0530

    ipodkvn

--- day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/day22/day23/day24/day25/day26/day27/day28/day29/day30/day31/else.py
+++ day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/day22/day23/day24/day25/day26/day27/day28/day29/day30/day31/else.py
@@ -6,3 +6,11 @@ elif b<a:
     print("b is greater than a")
 else:
     print("a is greater than a")
+
+#1
+a=200
+b=400
+if a>b:
+    print("a is greater than a")
+else:
+    print("b is greater than a")
