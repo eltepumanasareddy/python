21faa5a89e80ef45f66f63149a8ee322bc6871a2
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-03-03 22:26:00 +0530

    noise

--- day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/day22/day23/day24/day25/day26/day27/day28/day29/day30/day31/day32/shorthandif.py
+++ day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/day22/day23/day24/day25/day26/day27/day28/day29/day30/day31/day32/shorthandif.py
@@ -7,10 +7,16 @@ a=30
 b=25
 print("A") if a > b else print("B")
 
-#1
+#2
 a=10
 b=20
 bigger = a if a>b else b
 print("bigger is",bigger)
+
+
+#3
+a=50
+b=60
+print("A") if a>b else print("=") if a==b else print("B")
 
 
