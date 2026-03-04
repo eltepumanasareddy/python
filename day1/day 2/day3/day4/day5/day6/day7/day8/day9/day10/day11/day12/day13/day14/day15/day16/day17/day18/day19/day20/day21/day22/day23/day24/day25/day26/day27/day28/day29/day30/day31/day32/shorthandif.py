0a7b23a663eac953d841583ef752723ff6594aed
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-03-04 22:26:03 +0530

    ifandelse

--- day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/day22/day23/day24/day25/day26/day27/day28/day29/day30/day31/day32/shorthandif.py
+++ day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/day22/day23/day24/day25/day26/day27/day28/day29/day30/day31/day32/shorthandif.py
@@ -1,22 +1,22 @@
-a=200
-b=33
+a=225
+b=45
 if a > b:  print("a is greater than b")
 
 #1
-a=30
-b=25
+a=21
+b=20
 print("A") if a > b else print("B")
 
 #2
-a=10
-b=20
+a=20
+b=30
 bigger = a if a>b else b
 print("bigger is",bigger)
 
 
 #3
-a=50
-b=50
+a=60
+b=60
 print("A") if a>b else print("=") if a==b else print("B")
 
 
