4443eadb78f751c3e74aa8bfe1a09056db949706
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-03-07 16:59:23 +0530

    expected

--- day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/join.py
+++ day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/join.py
@@ -1,17 +1,17 @@
 list1=["a","b","c"]
-list2=[1,2,3]
+list2=[4,5,6]
 list3=list1 + list2
 print(list3)
 
 #1
 list1=["a","b","c",]
-list2=[1,2,3]
+list2=[4,5,6]
 for x in list2:
     list1.append(x)
 print(list1)
 
 #2
 list1=["a","b","c",]
-list2=[1,2,3]
+list2=[2,3,4]
 list1.extend(list2)
 print(list1)
