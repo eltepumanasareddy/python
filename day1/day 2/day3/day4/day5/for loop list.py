9dec50dc2068d49f850df87797f7ab698c38a2fb
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-03-08 22:03:49 +0530

    reddy

--- day1/day 2/day3/day4/day5/for loop list.py
+++ day1/day 2/day3/day4/day5/for loop list.py
@@ -1,9 +1,9 @@
-thislist = ["apple", "banana", "cherry"]
+thislist = ["guava,apple,banana"]
 for x in thislist:#for=one at a time
  print(x)
  
 #1
-list=["apple","banana","cherry"]
+list=["guava,apple,banana"]
 for i in range(len(thislist)):#used to store the index
  print(thislist[i])
 
