2f7ab4a9986c89b199e2548817f3253d56d169c8
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-03-10 22:28:53 +0530

    srgua

--- day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/day22/day23/day24/day25/day26/day27/day28/day29/day30/day31/day32/day33/day33/day35/nested if.py
+++ day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/day22/day23/day24/day25/day26/day27/day28/day29/day30/day31/day32/day33/day33/day35/nested if.py
@@ -5,3 +5,17 @@ if x>10:
   print("and also above 20!")
  else:
   print("but not above 20.")
+
+
+  #1
+  age = 25
+has_license = True
+
+if age >= 18:
+  if has_license:
+    print("You can drive")
+  else:
+    print("You need a license")
+else:
+  print("You are too young to drive")
+
