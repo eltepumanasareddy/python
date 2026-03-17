25193d12bf0bd4a9a336f4f846000686f949ad0f
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-03-17 22:53:43 +0530

    weekend

--- day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/day22/day23/day24/day25/day26/day27/day28/day29/day30/day31/day32/day33/day33/day39/match.py
+++ day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/day22/day23/day24/day25/day26/day27/day28/day29/day30/day31/day32/day33/day33/day39/match.py
@@ -14,3 +14,14 @@ match day:
         print("saturday")
     case 7:
         print("sunday")
+
+
+#1
+day = 5
+match day:
+    case 6:
+       print("Today is saturday")
+    case 7:
+        print("Today is sunday")
+    case 8:
+        print("Looking forward to the weekend")
