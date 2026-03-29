04372ebd7a2a8e90c435d08659331c10a66f1c95
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-03-29 23:08:28 +0530

    hsiwso

--- day1/breaks.py
+++ day1/breaks.py
@@ -68,3 +68,16 @@ def my_function():
 my_function()
 my_function()
 my_function()
+
+#2
+temp1 = 77
+celsius1 = (temp1 - 32) * 5 / 9
+print(celsius1)
+
+temp2 = 95
+celsius2 = (temp2 - 32) * 5 / 9
+print(celsius2)
+
+temp3 = 50
+celsius3 = (temp3 - 32) * 5 / 9
+print(celsius3)
