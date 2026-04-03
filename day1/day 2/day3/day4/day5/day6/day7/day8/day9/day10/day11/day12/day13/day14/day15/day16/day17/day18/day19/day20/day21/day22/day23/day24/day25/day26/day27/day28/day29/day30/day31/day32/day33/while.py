cd1e71d836518c4837d537e5b3bcdf6439ba2ea0
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-04-03 23:33:39 +0530

    we

--- day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/day22/day23/day24/day25/day26/day27/day28/day29/day30/day31/day32/day33/while.py
+++ day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/day16/day17/day18/day19/day20/day21/day22/day23/day24/day25/day26/day27/day28/day29/day30/day31/day32/day33/while.py
@@ -6,7 +6,7 @@ while i < 6:
 
 #1
 i = 1
-while i < 6:
+while i < 4:
   print(i)
   if i == 3:
     break
@@ -14,16 +14,16 @@ while i < 6:
 
 #2
 i = 0
-while i < 6:
+while i < 5:
   i += 1
-  if i == 3:
+  if i == 4:
     continue
   print(i)
 
 
 #3
 i=1
-while i < 6:
+while i < 5:
   print(i)
 else:
   print("i is no longer less than 6")
