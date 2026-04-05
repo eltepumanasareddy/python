11c1a47a89b019b04643b3579ab87816a6c5c201
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-04-05 22:43:15 +0530

    wnahe

--- day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/update .py
+++ day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/update .py
@@ -1,25 +1,25 @@
 #change tuple values
 x=("apple","banana","cherry")
 y=list(x)
-y[1]="kiwi"
+y[1]="orange"
 x=tuple(y)
 print(x)
 
 #add items
 thislist=("apple","banana","cherry")
 y=list(thislist)
-y.append("guvva")
+y.append("banana")
 thistuple=tuple(y)
 print(thistuple)
 
 #1
 thistuple=("apple","banana","cherry")
-y=("mango",)
+y=("cherry",)
 thistuple += y
 print(thistuple)
 
 #2
 thistuple = ("apple","banana","grapes")
 y = list(thistuple)
-y.remove("banana")
+y.remove("apple")
 thistuple = tuple(y)
