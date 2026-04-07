371d76c70067eb7d7dff60eb1f226d694a0b05ee
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-04-07 22:25:05 +0530

    dfg

--- MANASA/prob 2.py
+++ MANASA/prob 2.py
@@ -2,3 +2,14 @@ def my_function(*kids):
   print("The youngest child is " + kids[2])
 
 my_function("Emil", "Tobias", "Linus")
+
+
+
+#1
+def my_function(*args):
+  print("Type:", type(args))
+  print("First argument:", args[0])
+  print("Second argument:", args[1])
+  print("All arguments:", args)
+
+my_function("Emil", "Tobias", "Linus")
