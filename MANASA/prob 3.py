98a7f3dc63dc2ca8ed0214f1008a2c8eebfb5486
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-04-09 23:08:27 +0530

    norway

--- MANASA/prob 3.py
+++ MANASA/prob 3.py
@@ -5,3 +5,12 @@ my_function("Emil")
 my_function("Tobias")
 my_function()
 my_function("Linus")
+
+#1
+def my_function(country = "Norway"):
+  print("I am from", country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
