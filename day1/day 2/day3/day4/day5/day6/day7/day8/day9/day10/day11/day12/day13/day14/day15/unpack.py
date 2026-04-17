713c1ac40bf3ead98843ac0ca64e6bb6b541ad65
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-04-17 21:29:18 +0530

    wrum

--- day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/unpack.py
+++ day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/day14/day15/unpack.py
@@ -3,15 +3,15 @@ print(fruits)
 
 #1
 fruits = ("apple", "banana", "cherry")
-(green, yellow, red) = fruits
-print(green)
+(blue, yellow, red) = fruits
+print(blue)
 print(yellow)
 print(red)
 
 #2
 fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
-(green, yellow, *red) = fruits
-print(green)
+(blue, yellow, *red) = fruits
+print(blue)
 print(yellow)
 print(red)
 
