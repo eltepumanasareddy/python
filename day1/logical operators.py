44a3ff0c837bf4b1058816f2a0426f52085eb246
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-04-17 21:32:09 +0530

    eriofvv

--- day1/logical operators.py
+++ day1/logical operators.py
@@ -2,7 +2,7 @@ x=8
 print(x >4 and x<9)#Returns True if both statements are true
 
 x=6
-print(x <4 or x<10)#Returns True if one of the statements is true
+print(x <3 or x<10)#Returns True if one of the statements is true
 
 x=5
 print(not(x >4 and x<10))#returns False because not is used to reverse the result
@@ -10,7 +10,7 @@ print(not(x >4 and x<10))#returns False because not is used to reverse the resul
 
 #Test if a number is less than 5 or greater than 10:
 x = 6
-print(x < 7 or x > 15)
+print(x < 6 or x > 15)
 #Reverse the result with not:
 x=3
-print(not(5>3 and 5<10))
+print(not(5>6 and 5<10))
