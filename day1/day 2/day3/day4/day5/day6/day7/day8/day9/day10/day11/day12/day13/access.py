b845f51edfc0af0d2da9f90cc5bacb4140697a0a
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-04-24 22:53:13 +0530

    ydj

--- day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/access.py
+++ day1/day 2/day3/day4/day5/day6/day7/day8/day9/day10/day11/day12/day13/access.py
@@ -1,9 +1,9 @@
 thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
-print(thistuple[:4])
+print(thistuple[:5])
 
 #1
 thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
-print(thistuple[2:])
+print(thistuple[4:])
 
 #range of negative index
 thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
