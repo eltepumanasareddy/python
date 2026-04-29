8f9cfe2c4d1a9733972c420fe03dd5107a1fc249
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-04-29 22:32:37 +0530

    askad

---
+++ MANASA/prob6.py
@@ -0,0 +1,23 @@
+def myfunc1():
+  x = "Jane"
+  def myfunc2():
+    nonlocal x
+    x = "hello"
+  myfunc2()
+  return x
+
+print(myfunc1())
+
+#1
+x = "global"
+
+def outer():
+  x = "enclosing"
+  def inner():
+    x = "local"
+    print("Inner:", x)
+  inner()
+  print("Outer:", x)
+
+outer()
+print("Global:", x)
