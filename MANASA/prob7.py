a4470db88bdbfbba0e69a27d46957e8b4870574b
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-05-04 21:34:48 +0530

    adiiunk

--- MANASA/prob7.py
+++ MANASA/prob7.py
@@ -20,9 +20,9 @@ print(factorial(5))
 
 #2
 def fibonacci(n):
-  if n <= 1:
+  if n <= 2:
     return n
   else:
     return fibonacci(n - 1) + fibonacci(n - 2)
 
-print(fibonacci(7))
+print(fibonacci(5))
