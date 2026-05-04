6342d323b3d4df6f4b6b3e8a936177aa8ac616dc
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-05-04 21:33:19 +0530

    papppa

--- MANASA/prob8.py
+++ MANASA/prob8.py
@@ -1,10 +1,10 @@
-x = lambda a : a + 10
+x = lambda a : a + 9
 print(x(5))
 
 #1
 x = lambda a, b : a * b
-print(x(5, 6))
+print(x(6, 7))
 
 #2
 x = lambda a, b, c : a + b + c
-print(x(5, 6, 2))
+print(x(4, 8, 2))
