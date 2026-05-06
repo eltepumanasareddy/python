fb375925fbce7936c2dd72b9f5365ffd715664bf
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-05-06 20:51:46 +0530

    linus

--- MANASA/prob11.py
+++ MANASA/prob11.py
@@ -2,3 +2,22 @@ def myfunction():
   return "Have a great day!"
 
 print(myfunction.__name__)
+
+#1
+def changecase(n):
+  def changecase(func):
+    def myinner():
+      if n == 1:
+        a = func().lower()
+      else:
+        a = func().upper()
+      return a
+    return myinner
+  return changecase
+
+@changecase(1)
+def myfunction():
+  return "Hello Linus"
+
+print(myfunction())
+
