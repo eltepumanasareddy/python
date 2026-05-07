9c60934dfd3d8f1f10dc012e9f0a2f188e11b965
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-05-07 22:25:07 +0530

    sakesh

--- MANASA/prob10.py
+++ MANASA/prob10.py
@@ -1,34 +1,9 @@
-x = lambda a: a + 8
-print(x(6))
+x = lambda a: a + 50
+print(x(2))
 
 #1
 x = lambda a, b : a * b
-print(x(4, 5))
-
-#2
-x = lambda a, b, c: a + b + c
-print(x(5, 6, 7))
-
-#3
-def myfunc(n):
-  return lambda a : a * n
-
-mydoubler = myfunc(2)
-
-print(mydoubler(11))
-
-
-
-
-#6
-x = range(3, 10, 2)
-#display x:
-print(x)
-#convert to list to display the content of x:
-print(list(x))
-
-
-
+print(x(4, 9))
 
 
 
