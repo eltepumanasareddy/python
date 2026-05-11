8b7106fb5deb7a6a4a0cb537e99cc4c853794d5c
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-05-11 22:03:49 +0530

    rfkfvo

--- MANASA/prob10.py
+++ MANASA/prob10.py
@@ -27,16 +27,6 @@ mytripler = myfunc(6)
 
 print(mytripler(10))
 
-#5
-numbers = [1, 2, 3, 4, 5]
-doubled = list(map(lambda x: x * 2, numbers))
-print(doubled)
-
-#6
-numbers = [1, 2, 3, 4, 5, 6, 7, 8]
-odd_numbers = list(filter(lambda x: x % 2 != 0, numbers))
-print(odd_numbers)
-
 
 
 
