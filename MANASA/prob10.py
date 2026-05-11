2f23702ab98c34eeb8a7fa40791fc43666d0f3a7
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-05-11 21:56:03 +0530

    dkklekld

--- MANASA/prob10.py
+++ MANASA/prob10.py
@@ -32,6 +32,11 @@ numbers = [1, 2, 3, 4, 5]
 doubled = list(map(lambda x: x * 2, numbers))
 print(doubled)
 
+#6
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+odd_numbers = list(filter(lambda x: x % 2 != 0, numbers))
+print(odd_numbers)
+
 
 
 
