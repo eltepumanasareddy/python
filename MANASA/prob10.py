c7f03c4aa434bc54119bfb48d59fb276252e906d
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-05-11 22:06:01 +0530

    jxk

--- MANASA/prob10.py
+++ MANASA/prob10.py
@@ -1,14 +1,14 @@
-x = lambda a: a + 100
+x = lambda a: a + 70
 print(x(10))
 
 #1
 x = lambda a, b : a * b
-print(x(2, 9))
+print(x(2, 6))
 
 
 #2
 x = lambda a, b, c: a + b + c
-print(x(8, 10, 16))
+print(x(8, 12, 16))
 
 #3
 def myfunc(n):
