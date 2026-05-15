42febe47c19fdba1182ea63a247eb2d9477a51b3
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-05-15 20:44:04 +0530

    shjjl

--- MANASA/prob15.py
+++ MANASA/prob15.py
@@ -50,6 +50,6 @@ print("Global:", x)
 
 #1
 def my_function(*kids):
-  print("The youngest child is " + kids[2])
+  print("The youngest child is " + kids[1])
 
 my_function("Emil", "Tobias", "Linus")
