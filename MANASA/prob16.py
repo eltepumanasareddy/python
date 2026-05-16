cbbb94d112f8f6b2dc37e30c8743d77b8713a143
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-05-16 20:57:43 +0530

    eheoif

--- MANASA/prob16.py
+++ MANASA/prob16.py
@@ -8,3 +8,20 @@ def myfunction():
   return "Hello Sally"
 
 print(myfunction())
+
+#1
+def changecase(func):
+  def myinner():
+    return func().upper()
+  return myinner
+
+@changecase
+def myfunction():
+  return "Hello Sally"
+
+@changecase
+def otherfunction():
+  return "I am speed!"
+
+print(myfunction())
+print(otherfunction())
