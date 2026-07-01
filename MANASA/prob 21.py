9a3d2e88f0cc325c03c525eb8a066c6d6cbd3b99
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-07-01 18:55:32 +0530

    dhys

--- MANASA/prob 21.py
+++ MANASA/prob 21.py
@@ -58,3 +58,11 @@ def my_function(x,y):
     return x + y
 result = my_function(5,3)
 print(result)
+
+#11
+def my_function():
+    return["apple","banana","cherry"]
+fruits = my_function()
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
