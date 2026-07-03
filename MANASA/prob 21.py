794318bfa02b1e40fa6f7ba226f6fb3716aae7d3
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-07-03 21:39:49 +0530

    dsbu

--- MANASA/prob 21.py
+++ MANASA/prob 21.py
@@ -87,4 +87,9 @@ def my_function(a, b, /, *, c, d):
 result = my_function(5, 10, c = 15, d = 20)
 print(result)
 
+#15
+def my_function(*, name):
+  print("Hello", name)
+
+my_function(name = "Emil")
 
