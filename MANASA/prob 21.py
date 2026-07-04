1c51a5195bde6e8887a9bdab37c9dff3994f62d8
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-07-04 23:05:45 +0530

    wuyr

--- MANASA/prob 21.py
+++ MANASA/prob 21.py
@@ -84,7 +84,7 @@ my_function("Emil")
 def my_function(a, b, /, *, c, d):
   return a + b + c + d
 
-result = my_function(5, 10, c = 15, d = 20)
+result = my_function(5, 12, c = 15, d = 20)
 print(result)
 
 #15
