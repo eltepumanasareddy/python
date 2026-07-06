8251d3c17f51d43fbd880cf593b41e18e636c3d5
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-07-06 21:16:29 +0530

    wter

--- MANASA/prob 23.py
+++ MANASA/prob 23.py
@@ -1,4 +1,10 @@
+#1
 def my_function(a, b, /, *,c,d):
     return a+b+c+d
 result = my_function(5,10, c=15,d=20)
 print(result)
+
+#2
+def my_function(*kids):
+    print("The youngest child is " +kids[2])
+my_function("Emil","Tobias", "Linus")
