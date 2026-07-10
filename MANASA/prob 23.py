020f1300eb34ba4247547cd420c9fa7479709cd3
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-07-10 22:50:25 +0530

    fsrt

--- MANASA/prob 23.py
+++ MANASA/prob 23.py
@@ -70,3 +70,12 @@ def my_function(fname, lname):
 
 person = {"fname": "Emil", "lname": "Refsnes"}
 my_function(**person)
+
+#10
+def my_function(username, **details):
+  print("Username:", username)
+  print("Additional details:")
+  for key, value in details.items():
+    print("  ", key + ":", value)
+
+my_function("emil123", age = 25, city = "Oslo", hobby = "coding")
