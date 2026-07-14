b01039e72e30db9a839d6f6dd49cc87d3482b19b
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-07-14 23:03:42 +0530

    rfew

--- MANASA/prob 3.py
+++ MANASA/prob 3.py
@@ -57,3 +57,13 @@ def countdown(n):
 
 countdown(4)
 
+#6
+def factorial(n):
+  # Base case
+  if n == 0 or n == 1:
+    return 1
+  # Recursive case
+  else:
+    return n * factorial(n - 1)
+
+print(factorial(5))
