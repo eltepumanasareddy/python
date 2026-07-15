2e045841606d8258e0b3847cb1bf73c70eddf609
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-07-15 21:10:17 +0530

    sbjkd

--- MANASA/prob21.py
+++ MANASA/prob21.py
@@ -14,3 +14,15 @@ def myfunction():
   return "Have a great day!"
 
 print(myfunction.__name__)
+
+#2
+def changecase(func):
+  def myinner(*args, **kwargs):
+    return func(*args, **kwargs).upper()
+  return myinner
+
+@changecase
+def myfunction(nam):
+  return "Hello " + nam
+
+print(myfunction("John"))
