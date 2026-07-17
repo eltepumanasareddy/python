5f4e01e0b3af873c49413c96bd2ce3151036d305
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-07-17 23:02:43 +0530

    cbjk

--- MANASA/prob21.py
+++ MANASA/prob21.py
@@ -41,10 +41,16 @@ print(x(5, 6, 2))
 #6
 def myfunc(n):
   return lambda a : a * n
-
 mydoubler = myfunc(2)
-
 print(mydoubler(11))
+#7
+def myfunc(n):
+  return lambda a : a * n
+
+mytripler = myfunc(3)
+
+print(mytripler(11))
+
 
 
 
