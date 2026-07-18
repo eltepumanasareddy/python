9427ec30d4f21ec4d4136bf459f8a5d82ed43690
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-07-18 20:43:55 +0530

    sghyt

--- MANASA/prob21.py
+++ MANASA/prob21.py
@@ -61,6 +61,10 @@ mydoubler = myfunc(2)
 mytripler = myfunc(3)
 print(mydoubler(11))
 print(mytripler(11))
+#10
+numbers = [1, 2, 3, 4, 5]
+doubled = list(map(lambda x: x * 2, numbers))
+print(doubled)
 
 
 
