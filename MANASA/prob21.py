6fbfa802bc8bf91b7d4de8ae4dfbc115333bf861
eltepumanasareddy <eltepumanasareddy@gmail.com>
2026-07-19 22:34:28 +0530

    gwyht

--- MANASA/prob21.py
+++ MANASA/prob21.py
@@ -74,6 +74,16 @@ def countdown(n):
     countdown(n - 1)
 
 countdown(5)
+#12
+def factorial(n):
+  # Base case
+  if n == 0 or n == 1:
+    return 1
+  # Recursive case
+  else:
+    return n * factorial(n - 1)
+
+print(factorial(5))
 
 
 
